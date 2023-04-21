eef13c71c1965158d30093c0d79d7d0a73432a09
jmpollar <103337804+jmpollar@users.noreply.github.com>
2023-04-21 14:50:59 -0700

    lab 4 complete

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,35 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Pseudocoding and Problem-solving exercise. 
+//index.html includes details of pseudocode for a computer game and an everyday task
+// Author: James Pollard
+// Date: April 20 2023
+/*Steps for Rock Paper Scissors*/
+//Request input from player (1: rock, 2: paper, 3: scissors)
+  //if(input>3 || input < 1) {error; re-request input}
+  //assign to int variable playerChoice
+//have computer produce RNG between 1 and 3
+  //assign this to int variable computerChoice
+//if statements for outcomes:
+  //if(computer = rock and player = scissors)
+    //computer wins!
+  //if(computer = paper and player = scissors)
+    //player wins!
+  //if(computer = scissors and player = scissors)
+    //tie!
+  //if(player = rock and computer = rock)
+    //tie!
+  //if(player = rock and computer = paper)
+    //computer wins!
+  //if(player = rock and computer = scissors)
+    //player wins!
+  //if(player = paper and computer = rock)
+    //player wins!
+  //if(player = paper and computer = paper)
+    //tie!
+  //if(player = paper and computer = scissors)
+    //computer wins!
+//Show graphics based on computer and player selection
+//print result of if cluster above
+//program complete
 
 // Constants
 
