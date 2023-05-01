adbef3539a9431ce5be274488726c74debae5671
jmpollar <103337804+jmpollar@users.noreply.github.com>
2023-05-01 13:12:38 -0700

    linking and lab 6 complete

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,29 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Use JavaScript to explore arrays and objects
+// Author: James Pollard
+// Date: 4/27/2023
 
 // Constants
 
 // Functions
+//declare myTransport array
+myTransport = ["car"];
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+currentYear = 2023;
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+//create an object for this primary transport
+myMainRide = {
+  year: 2022,
+  make: "Honda",
+  model: "Civic",
+  color: "Blue",
+  //internal function to determine age val
+  age: function() {
+    return currentYear - this.year;
+  }
+};
 
-// let's get this party started
-main();
+//print out my transports
+document.writeln("I move with my " + myTransport + ".<br><br>");
+//print out myMainRide
+document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre");
+document.writeln("<pre>Age: ", myMainRide.age(), "</pre>");
