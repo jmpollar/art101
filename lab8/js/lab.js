bae8e5d827764f32c4586f73333c9f68eeed60b3
jmpollar <103337804+jmpollar@users.noreply.github.com>
2023-05-05 11:29:01 -0700

    lab done

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -4,14 +4,14 @@
 
 // Constants
 //declare array
-numArr = [0, 3, -2, -99, -4, -5, 95, 37, 59, 84775, 20849, 2];
+numArr = [0, 3, -6, -99, -81, -5, 95, 37, 34624312432, 84775, 20849, 2];
 
 // Functions
 //create a named function that will take a single parameter, do a calculation on it, and return the result
 function ArrayExponentials(val) {
   //square the param
   var results = val * val;
-  return (results + " equals " + val + "^2.");
+  return (" " + results + " equals " + val + "^2");
 }
 
 //use map on array using the function as a callback to operate on all the numbers in the array
@@ -24,15 +24,27 @@ divByThreeTester = numArr.map(function (x) {
   var results = x % 3;
   if (results == 0) {
     //is divisible by 3
-    return x;
+    return " " + x;
   }
   else {
     //say it isn't divisible by 3
-    return ("Indivisible by 3.")
+    return (" Indivisible by 3")
   }
+});
+
+//additional function to check if the squared values are divisible by 3
+numCuber = numArr.map(function (x) {
+  var results = x*x*x;
+  return " " + results;
 });
 
 //test!
 console.log(numArr);
 console.log(squaredArray);
 console.log(divByThreeTester);
+
+
+//add to index.html
+var outputEl = document.getElementById("output"); 
+mapResults = "<h3>Original Array:<br></h3>" + numArr + "<h3><br><br>Squared Array:<br></h3>" + squaredArray + "<h3><br><br>Cubed Array:<br></h3>" + numCuber + "<h3><br><br>Divisible-by-Three-Tester:<br></h3>" + divByThreeTester;
+outputEl.innerHTML = mapResults; // put your results here
