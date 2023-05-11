8145b0341460d43a2ca354ac0462be9c81fb7dd0
jmpollar <103337804+jmpollar@users.noreply.github.com>
2023-05-11 11:59:29 -0700

    update 511 experiment

--- js/experiment2.js
+++ js/experiment2.js
@@ -1,9 +1,9 @@
-var buttonEl = document.createElement('button');
+var buttonEl = document.getElementById('button-new');
 buttonEl.innerHTML = "Hello World!";
 
-var pEl = document.getElementById('pNew');
+var h1El = document.getElementById('h1-new');
 
-pEl.prepend(buttonEl);
+h1El.appendChild(buttonEl);
 
 buttonEl.addEventListener("click", function(){
     nombre = prompt("Enter your name: ");
@@ -13,6 +13,11 @@ buttonEl.addEventListener("click", function(){
     document.body.style.backgroundColor = arr[rand];
     buttonEl.style.backgroundColor = arr[rand2];
     buttonEl.innerHTML = "Hello " + nombre + "!";
+    arrBord = ["dotted ", "dashed ", "solid ", "double ", "groove ", "ridge ", "inset ", "outset "];
+    rand3 = Math.floor(Math.random() * (arrBord.length +1));
+    borderStr = "30px " + arrBord[rand3] + arr[rand2];
+    console.log(borderStr);
+    document.body.style.border = borderStr;
 });
 
 
