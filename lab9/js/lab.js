addcb1b3fe50d906e796f5360d47fc768be9a2e2
jmpollar <103337804+jmpollar@users.noreply.github.com>
2023-05-10 19:20:57 -0700

    more lab 9 prog

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,23 +3,23 @@
 // Date: 5/10/2023
 
 //find output div and assign to var outputEl
-outputEl = getElementById('output');
+outputEl = document.getElementById('output');
 
 //create new element with document.createElement("p") and assign it ot a variable new1El
 new1El = document.createElement('p');
 
 //change the html attribute of new1El to say something new by using setAttribute (working?)
-new1El.setAttribute('id', 'challenge-p');
+new1El.id = "challenge-p";
 
 //create another element and assign it to a variable new2El
 new2El = document.createElement('img');
 
 //change the html attribute of new2El to say something else (working?)
-new2El.setAttribute('id', 'problems-p');
+new2El.id = "problems-p";
 
 //append new1El and new2El to outputEl
 outputEl.appendChild(new1El);
 outputEl.appendChild(new2El);
 
 //change css attributes of at least 2 elements on the page
-new1El.setAttribute('font-size', '100px');
+new1El.style.color = red;
