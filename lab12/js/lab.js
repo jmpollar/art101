d1f570b70729082305149ee3abce55b152d53171
jmpollar <103337804+jmpollar@users.noreply.github.com>
2023-05-19 11:02:58 -0700

    finalized task3 lab12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -3,7 +3,7 @@
 // Date: 5/18/23
 
 //create function sortingHat() to take a string as an argument and uses mod to assign harry potter house based on length%4 and returns a string based on that, all run through a click event listener on a button
-
+//harry potter font from https://www.fontspace.com/harry-potter-font-f3199
 function sortingHat(str)
 {
   //cast input to string
@@ -35,10 +35,32 @@ function sortingHat(str)
 //create event listener attached to #button that gets the value of #input and assigns it to var name;
 //sends name as param to sortingHat
 //appends new paragraph to output that displays this return value
+//house colors ripped from qualitylogoproducts.com
 $("#button").click(function() {
   var name = $("#input").val();
   console.log(name);
   var house = sortingHat(name);
-  var str2Print = "<p id=sort-p>The Sorting Hat has sorted you into " + house + ".</p>";
+  console.log(house);
+  var str2Print = "<p id=sort-p>"+name+": The Sorting Hat has sorted you into " + house + ".</p>";
   $("#output").append(str2Print);
+  if(house === "Gryffindor: House 0")
+  {
+    $("#hp-house-body").css("background-color", "#740001");
+    $("#content").css("background-color", "#EEBA30")
+  }
+  else if(house === "Ravenclaw: House 1")
+  {
+    $("#hp-house-body").css("background-color", "#5D5D5D");
+    $("#content").css("background-color", "#0E1A40");
+  }
+  else if(house === "Slytherin: House 2")
+  {
+    $("#hp-house-body").css("background-color", "#AAAAAA");
+    $("#content").css("background-color", "#2A623D");
+  }
+  else if(house === "Hufflepuff: House 3")
+  {
+    $("#hp-house-body").css("background-color", "#60605C");
+    $("#content").css("background-color", "#FFDB00");
+  }
 })
