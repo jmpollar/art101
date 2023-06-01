d4e06baceb7761aa2b0d22f858ae4054016cbe9d
jmpollar <103337804+jmpollar@users.noreply.github.com>
2023-06-01 00:11:48 -0700

    final lab 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -5,7 +5,7 @@
 //using the core $.ajax() method
 function getStuff(){$.ajax({
   // The URL for the request (from the api docs)
-  url:"https://catfact.ninja/fact",
+  url:"https://api.weather.gov/points/36.9741,-122.0308",
   // Whether this is a POST or GET request
   type:"GET",
   // The data to send (will be converted to a query string)
@@ -13,9 +13,19 @@ function getStuff(){$.ajax({
 })
 //  All the action goes here
 .done(function(data){
+  var datStr;
   //send the data to console and output HTML
-  console.log("Cat Fact!</br>" + data.fact);
-  $("#get-stuff").html("Cat Fact!</br>" + data.fact);
+  for(ind in data.properties.relativeLocation)
+      {
+        datStr+=String("</br>" + ind + ": " + data[ind])
+        for(ind2 in data.properties.relativeLocation[ind])
+        {
+          datStr+=String("</br>     "+ind2 + ": " + data.properties.relativeLocation[ind][ind2]); //one method
+        }
+          
+      }
+  console.log("Cat Fact!</br>" + datStr);
+  $("#get-stuff").html("</br>" + datStr);
 })
 }
 //call the function on button click
