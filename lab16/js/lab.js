81dbc675837fea5c5695003f51306fe0ec076d91
jmpollar <103337804+jmpollar@users.noreply.github.com>
2023-06-02 12:01:02 -0700

    lab 16 progress

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,29 +1,34 @@
-// index.js - Use JavaScript to explore arrays and objects
+// index.js - Use AJAX and JSON to parse through URL data and print it to the page
 // Author: James Pollard
-// Date: 4/27/2023
+// Date: 6/1/2023
 
-// Constants
-
-// Functions
-//declare myTransport array
-myTransport = ["car"];
-
-currentYear = 2023;
-
-//create an object for this primary transport
-myMainRide = {
-  year: 2022,
-  make: "Honda",
-  model: "Civic",
-  color: "Blue",
-  //internal function to determine age val
-  age: function() {
-    return currentYear - this.year;
+// Using the core $.ajax() method
+$.ajax({
+  // The URL for the request (from the api docs)
+  url: "https://xkcd.com/612/info.0.json",
+  // The data to send (will be converted to a query string)
+  data: {},
+  // Whether this is a POST or GET request
+  type: "GET",
+  // The type of data we expect back
+  dataType : "json",
+  // What do we do when the api call is successful
+  //   all the action goes in here
+  success: function(data) {
+      // do stuff
+      console.log(data);
+      var datStr;
+      //send the data to console and output HTML
+      for(ind in data)
+          {
+            datStr+=String("</br>" + ind + ": " + data[ind])
+          }
+      $("#output-p").html("</br>" + datStr);
+      $("#output").append("<img src =" + data.img + ">");
+  },
+  // What we do if the api call fails
+  error: function (jqXHR, textStatus, errorThrown) { 
+      // do stuff
+      console.log("Error:", textStatus, errorThrown);
   }
-};
-
-//print out my transports
-document.writeln("I move with my " + myTransport + ".<br><br>");
-//print out myMainRide
-document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre");
-document.writeln("<pre>Age: ", myMainRide.age(), "</pre>");
+})
